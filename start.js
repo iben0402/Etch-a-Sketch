76fd540ed40dfdd54e6e8a514047fb76cb1ef4dc
iben0402 <81482747+iben0402@users.noreply.github.com>
2023-03-30 10:48:15 +0200

    changing size of grid

--- start.js
+++ start.js
@@ -2,18 +2,38 @@ function changeColor() {
     this.classList.add('coloured');
 }
 
-const container = document.getElementById("container")
-
-for (let index = 0; index < 16; index++) {
-    var mainDiv = document.createElement("div");
-    mainDiv.className = "row"
-    for (let j = 0; j < 16; j++) {
-        var div = document.createElement("div")
-        div.className = "column"
-        div.addEventListener('mouseenter', changeColor)
-        mainDiv.appendChild(div)
-    }
-    container.appendChild(mainDiv)
+function setGrid(size) {
     
+    const container = document.getElementById("container")
+
+    for (let index = 0; index < size; index++) {
+        var mainDiv = document.createElement("div");
+        mainDiv.className = "row"
+        for (let j = 0; j < size; j++) {
+            var div = document.createElement("div")
+            div.className = "column"
+            div.addEventListener('mouseenter', changeColor)
+            mainDiv.appendChild(div)
+        }
+        container.appendChild(mainDiv)
+    }
 }
+
+function changeGrid() {
+    let newSize = prompt("Enter new size of the grid (max is 960)");
+    if(newSize>960) {
+        changeGrid()
+    }
+    else {
+        const container = document.getElementById("container")
+        container.innerHTML = "";
+        setGrid(newSize)
+    }
+}
+
+const changeSizeButton = document.getElementById("change_size");
+changeSizeButton.addEventListener("click", changeGrid)
+
+setGrid(16)
+
 
